eac35600ea0d6c1e31809f1289f6b15a31d2fe35
Nabi Ahmad <cs.nabiahmad@gmail.com>
2024-10-31 13:34:26 +0500

    Initial Commit

---
+++ app/src/main/java/com/chimeup/android/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.chimeup.android.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val AquaBlue= Color(0xFF00C1D4)
+val SoftViolet= Color(0xFF9D7DE8)
+val OffWhite= Color(0xFFF5F5F5)
+val MintGreen= Color(0xFF7BE495)
+val DarkSlate= Color(0xFF2D2D2D)
+val LightLavender= Color(0xFFE6EAF9)
+val PaleBlue= Color(0xFFDFF6F5)
